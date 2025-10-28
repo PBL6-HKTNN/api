6f4421fa661096f4bfc5a87d3e7c7554ec0f21a8
imlawcs <daolehanhnguyen@gmail.com>
2025-10-28 13:38:25 +0700

    feat: add swagger into filestorage service

--- src/Services/FileStorage/Codemy.FileStorage.API/Program.cs
+++ src/Services/FileStorage/Codemy.FileStorage.API/Program.cs
@@ -17,20 +17,25 @@ builder.Services.Configure<CloudinarySettings>(
 builder.Services.AddScoped<IFileService, CloudinaryService>();
 builder.Services.AddScoped<FileStorageAppService>();
 
-
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure Swagger middleware
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
